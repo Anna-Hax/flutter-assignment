e52f44105c099b206b97dd200b8edae3dcc3c673
Anna-Hax <dhruvi.purohit06@gmail.com>
2025-06-06 17:35:38 +0530

    feat: auth frontend

---
+++ flutter_app/lib/bloc/auth_bloc.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_app/bloc/auth_event.dart';
+import 'package:flutter_app/bloc/auth_state.dart';
+import 'package:flutter_app/data/auth_service.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final AuthService authService;
+
+  AuthBloc(this.authService) : super(AuthInitial()) {
+    on<LoginRequested>((event, emit) async {
+      emit(AuthLoading());
+      try{
+        final authModel = await authService.login(
+          email: event.email,
+          password: event.password,
+        );
+        emit(AuthAuthenticated(authModel.email));
+      } catch (error) {
+        emit(AuthError("Login failed"));
+      }
+    });
+
+    on<LogoutRequested>((event, emit) async {
+      await authService.logout();
+      emit(AuthUnauthenticated());
+    });
+
+    on<CheckLoginStatus>((event, emit) async {
+      final loggedIn = await authService.isLoggedIn();
+      if(loggedIn){
+        final token = await authService.getToken();
+        emit(AuthAuthenticated(token ?? ""));
+      } else {
+        emit(AuthUnauthenticated());
+      }
+    });
+  }
+}
---
+++ flutter_app/lib/data/auth_service.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/auth_model.dart';
+
+class AuthService {
+
+  final Dio _dio = Dio();
+
+  Future<AuthModel> login({required String email, required String password}) async {
+    final response = await _dio.post('http://localhost:8080/auth/login',
+      data: {
+        "email": email,
+        "password": password,
+      },
+    );
+
+    if (response.statusCode == 200){
+      final model = AuthModel.fromJson(response.data);
+      final pref = await SharedPreferences.getInstance();
+      await pref.setString('token', model.token);
+      return model;
+    } else {
+      throw Exception("Login failed");
+    }
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove('token');
+  }
+
+  Future<bool> isLoggedIn() async{
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') != null;
+  }
+
+  Future<String?> getToken() async{
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token');
+  }
+}
